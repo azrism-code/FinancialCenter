93adbe4adf2330b8ca1f388b7f130e0eb0cc6d96
azrism-code <azrism@gmail.com>
2026-09-25 10:12:51 +0300

    Refresh PWA cache for v1.6.1

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='financial-shell-v12';const ASSETS=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./quotes.js','./firebase.js','./firebase-config.js'];
+const CACHE='financial-shell-v13';const ASSETS=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png','./quotes.js','./firebase.js','./firebase-config.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!=='GET'||u.origin!==location.origin||!ASSETS.some(a=>new URL(a,location.href).pathname===u.pathname))return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c))}return r}).catch(()=>caches.match(e.request)))});
